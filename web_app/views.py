4c266606d18ae1625982cb78cb8fdeaccc703e3d
Vladislav <sgipovskoi@gmail.com>
2015-07-18 23:49:51 +0600

    table v2

--- web_app/views.py
+++ web_app/views.py
@@ -1,10 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
 from django.shortcuts import render, render_to_response, RequestContext ,loader,HttpResponse
 from .models import Tweets
+import tweepy
+import time
+import sqlite3
 
-# Create your views here.
 
 def home(request):
-	latest_question_list = Tweets.objects.order_by('-tweet_text')[:5]
+	consumer_key = 'OUbN0hOILkmGloZue1WglHEdQ'
+	consumer_secret = 'uc37EPwzBJDMQ4qYuDyGIK9cb2akIdu9dStT0A0ktK9VqKedmF'
+	access_token = '1545853322-YL8N6URUxoTQ47LHI8sYnOQA8NoS42bwEYOX46w'
+	access_token_secret = 'WFoJ8UirQpk0UWxlSzjUwWKHSBJcs1KFuaX2e1W25frxS'
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth)
+	results = api.search(q = "#Путин", count = 15)
+	for result in results:
+			hash_list=[]
+			for hashtags in result.entities['hashtags']:
+				hash_list.append(hashtags['text'])
+				hash_str= ",".join(hash_list)
+
+			q = Tweets(tweet_id=result.id_str,tweet_text=result.text,hashtags=hash_str,created_at=str(result.created_at))
+			q.save()
+
+	latest_question_list = Tweets.objects.order_by('-tweet_text')
 	template = loader.get_template('web_app/index.html')
 	context = RequestContext(request, {'latest_question_list': latest_question_list,})
 	return HttpResponse(template.render(context))
